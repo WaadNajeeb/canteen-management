c14a690841bae8eddd37e42ee51369f5da9ca613
WaadNajeeb <waad.najeeb@education.nsw.gov.au>
2025-06-15 09:10:26 +1000

    create the init setup for the for frontend

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,29 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { ROUTES } from './app/AppRoutes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(BrowserModule),
+    provideRouter(ROUTES),
+    // { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    // { provide: HTTP_INTERCEPTORS, useClass: XsrfInterceptor, multi: true },
+    // { provide: RECAPTCHA_V3_SITE_KEY, useValue: APIKeys.RecaptchaSiteKey },
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+
+  ]
+})
+  .catch(err => console.error(err));
+
+
